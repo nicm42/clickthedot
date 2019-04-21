7fbd2833c4ca10e9ab4a5d084f8e3baa62edfc44
Nic Mayer <nicmayer42@gmail.com>
2019-04-21 14:03:24 +0100

    Set up basic animation and event listener bits

--- clickthedot.js
+++ clickthedot.js
@@ -1,3 +1,19 @@
+//add a start button
+let startBtn = document.createElement('button');
+startBtn.textContent = "Start";
+document.body.appendChild(startBtn);
+
+//add the circle
 let dotDiv = document.createElement('div');
 dotDiv.className = 'dot';
 document.body.appendChild(dotDiv);
+
+//When the user clicks the button, make the dot grow
+startBtn.addEventListener('click', event => {
+	dotDiv.classList.add('grow');
+})
+
+//When the user clicks the dot, stop it growing
+dotDiv.addEventListener('click', event => {
+	dotDiv.classList.remove('grow');
+})
