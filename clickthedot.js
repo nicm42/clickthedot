f132a6d798a4ec579589a6d9bef32ef9925e0502
Nic Mayer <nicmayer42@gmail.com>
2019-04-21 16:18:14 +0100

    Done basic game functionality

--- clickthedot.js
+++ clickthedot.js
@@ -1,12 +1,14 @@
+let container = document.getElementById('container');
+
 //add a start button
 let startBtn = document.createElement('button');
 startBtn.textContent = "Start";
-document.body.appendChild(startBtn);
+document.body.insertBefore(startBtn, container);
 
 //add the circle
 let dotDiv = document.createElement('div');
 dotDiv.className = 'dot';
-document.body.appendChild(dotDiv);
+container.appendChild(dotDiv);
 
 //When the user clicks the button, make the dot grow
 startBtn.addEventListener('click', event => {
@@ -14,6 +16,14 @@ startBtn.addEventListener('click', event => {
 })
 
 //When the user clicks the dot, stop it growing
+//And keep a count of the width
+let widthWhenClicked;
 dotDiv.addEventListener('click', event => {
+	widthWhenClicked = dotDiv.offsetWidth;
 	dotDiv.classList.remove('grow');
+	//Work out how much bigger the dot was when clicked in comparison to when it started
+	//Add text saying this to the page
+	let startWidth = 100;
+	let ratio = widthWhenClicked / startWidth;
+	container.insertAdjacentHTML('beforebegin','<div id="result">' + `Result is ${ratio}` + '</div>')
 })
