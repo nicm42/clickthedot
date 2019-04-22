e4751ce6d7e52dbf520495e38e16c53a7f251744
Nic Mayer <nicmayer42@gmail.com>
2019-04-22 20:57:00 +0100

    Made it more playable and changed the arrow functions to actual functions for readability

--- clickthedot.js
+++ clickthedot.js
@@ -1,29 +1,44 @@
-let container = document.getElementById('container');
+/* TODO
+	dot keep its size when it's clicked
+	maybe show its original size so you can see the difference
+	then need reset button
+	if it grows to 300px then keep it that size
+	and add message to say they didn't click
+ */
 
-//add a start button
-let startBtn = document.createElement('button');
-startBtn.textContent = "Start";
-document.body.insertBefore(startBtn, container);
+const container = document.getElementById('container');
 
 //add the circle
-let dotDiv = document.createElement('div');
+const dotDiv = document.createElement('div');
 dotDiv.className = 'dot';
 container.appendChild(dotDiv);
 
-//When the user clicks the button, make the dot grow
-startBtn.addEventListener('click', event => {
-	dotDiv.classList.add('grow');
-})
+//When the user clicks the dot, make the dot grow
+dotDiv.addEventListener('click', growDot);
 
-//When the user clicks the dot, stop it growing
-//And keep a count of the width
-let widthWhenClicked;
-dotDiv.addEventListener('click', event => {
+function growDot(){
+	//first remove previous result (if there is one)
+	dotDiv.classList.add('grow');
+	//When the user clicks the dot again, stop it growing
+	//And keep a count of the width
+	let widthWhenClicked;
+	dotDiv.addEventListener('click', stopDot, false);
+}
+
+function stopDot(event){
 	widthWhenClicked = dotDiv.offsetWidth;
 	dotDiv.classList.remove('grow');
 	//Work out how much bigger the dot was when clicked in comparison to when it started
 	//Add text saying this to the page
 	let startWidth = 100;
 	let ratio = widthWhenClicked / startWidth;
-	container.insertAdjacentHTML('beforebegin','<div id="result">' + `Result is ${ratio}` + '</div>')
-})
+	//Text will depend on how close to 2.0 ratio is
+	let resultTextStart = "Missed!";
+	if(ratio >= 1.9 && ratio <= 2.1){
+		resultTextStart = "Close!";
+	}
+	const result = document.getElementById('result');
+	result.innerHTML = `${resultTextStart}  When you clicked it, the dot was  ${ratio} times its original size`;
+	//now stop this event, otherwise it'll fire twice for every click
+	dotDiv.removeEventListener('click', stopDot, false);
+}
