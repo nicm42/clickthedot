dec2336726ad0146a0f7a5edd5de5e99bbfaf95a
Nic Mayer <nicmayer42@gmail.com>
2019-04-24 20:06:33 +0100

    Added reset button

--- clickthedot.js
+++ clickthedot.js
@@ -1,31 +1,37 @@
 /* TODO
-	need reset button
-	leave space for the result message, otherwise dot moves down on click
 	maybe show its original size so you can see the difference
-	if it grows to 3 times size then keep it that size
-	and add message to say they didn't click
 	take a random amount of time to grow it
  */
 
 const container = document.getElementById('container');
+const result = document.getElementById('result');
+const resetButton = document.getElementById('reset');
 
 //add the circle
 const dotDiv = document.createElement('div');
 dotDiv.className = 'dot';
 container.appendChild(dotDiv);
 
-//set up the initial size of the dot
-let size = getRandomIntInclusive(50, 150);
-let size3 = size * 3;
-dotDiv.style.width = size + 'px';
-dotDiv.style.height = size + 'px';
+let initialSize = setUpDot();
 
-//When the user clicks the dot, make the dot grow
-dotDiv.addEventListener('click', growDot);
+resetButton.addEventListener('click', resetDot);
+
+function setUpDot(){
+	let size = getRandomIntInclusive(50, 100);
+	dotDiv.style.width = size + 'px';
+	dotDiv.style.height = size + 'px';
+	//When the user clicks the dot, make the dot grow
+	dotDiv.addEventListener('click', growDot);
+	//if the dot has got to 3 times the size, add a message to result
+	//we can just re-use stopDot for this
+	dotDiv.addEventListener('transitionend', stopDot);
+	return size;
+}
 
 function growDot(){
 	dotDiv.classList.add('transition');
 	//set up the transition limit - 3 times the size should be enough
+	let size3 = initialSize * 3;
 	dotDiv.style.width = size3 + 'px';
 	dotDiv.style.height = size3 + 'px';
 	//When the user clicks the dot again, stop it growing
@@ -40,24 +46,32 @@ function stopDot(event){
 	let heightWhenClicked = computedStyle.getPropertyValue('height');
 	dotDiv.style.width = widthWhenClicked;
 	dotDiv.style.height = heightWhenClicked;
-	//stop it from growing by setting transition-property to none, but using a class
-	dotDiv.classList.add('noTransition');
+	//stop it from growing any more by removing the transition
+	dotDiv.classList.remove('transition');
 	//Work out how much bigger the dot was when clicked in comparison to when it started
 	//Add text saying this to the page
 	//widthWhenClicked = heightWhenClicked so we can just use one
 	//however it's a number + px, so we need to strip off the px part
-	let startWidth = size;
 	let widthNumber = widthWhenClicked.substring(0,widthWhenClicked.length-2);
-	let ratio = (widthNumber / startWidth).toFixed(2);
+	let ratio = (widthNumber / initialSize).toFixed(2);
 	//Text will depend on how close to 2.0 ratio is
 	let resultTextStart = "Missed!";
 	if(ratio >= 1.9 && ratio <= 2.1){
 		resultTextStart = "Close!";
 	}
-	const result = document.getElementById('result');
-	result.innerHTML = `${resultTextStart}  When you clicked it, the dot was  ${ratio} times its original size`;
+	result.innerHTML = `${resultTextStart}  The dot is now  ${ratio} times its original size`;
+	//show reset button
+	resetButton.style.visibility = 'visible';
 	//now stop this event, otherwise it'll fire twice for every click
 	dotDiv.removeEventListener('click', stopDot, false);
+}
+
+function resetDot(){
+	//remove the text in result, hide the rest button
+	result.innerHTML = "&nbsp;";
+	resetButton.style.visibility = 'hidden';
+	//make the dot smaller again
+	initialSize = setUpDot();
 }
 
 //from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
