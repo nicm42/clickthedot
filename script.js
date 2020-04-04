3a7e5b84de7d0b3ab2a26cb1695e614b241b9efd
Nic Mayer <nicmayer42@gmail.com>
2020-04-04 18:09:56 +0100

    Added message when shape finishes growing

--- script.js
+++ script.js
@@ -18,13 +18,15 @@ function createShape() {
   });
 
   if ($(this).val() === 'circle' || $(this).val() === 'square') {
+    console.log($(this).val());
     $('.shape').css({
       'width': size,
       'height': size
     });
   }
 
-  if ('triangle') {
+  if ($(this).val() === 'triangle') {
+    console.log($(this).val());
     $('.shape').css({
       'border-left-width': size / 2,
       'border-right-width': size / 2,
@@ -45,7 +47,9 @@ function growShape(shape, size) {
     $('.shape').animate({
       'width': size * 3,
       'height': size * 3
-    }, transitionTime * 1000);
+    }, transitionTime * 1000, function () {
+      stopShape(shape, size);
+    });
   }
 
   if (shape === 'triangle') {
@@ -53,8 +57,36 @@ function growShape(shape, size) {
       'border-left-width': size / 2 * 3,
       'border-right-width': size / 2 * 3,
       'border-bottom-width': size * 3
-    }, transitionTime * 1000);
+    }, transitionTime * 1000, function () {
+      stopShape(shape, size);
+    });
   }
+}
+
+function stopShape(shape, initialSize) {
+  var finalSize;
+
+  if (shape === 'circle' || shape === 'square') {
+    finalSize = $('.shape').width();
+  }
+
+  if (shape === 'triangle') {
+    finalSize = parseInt($('.shape').css('border-bottom-width')); //borders return with a px
+  }
+
+  var ratio = (finalSize / initialSize).toFixed(1);
+  var resultTextStart = "Missed!";
+
+  if (ratio == 1.9 || ratio == 2.1) {
+    resultTextStart = "Close!";
+  }
+
+  if (ratio == 2.0) {
+    resultTextStart = "Well done!";
+  }
+
+  console.log(initialSize, finalSize);
+  $('#result').html(resultTextStart + '<br>The dot is now ' + ratio + ' times its original size');
 } //from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 
