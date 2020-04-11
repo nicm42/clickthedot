18e3b2a0a2446063d55184a424df32b3efb60a93
Nic Mayer <nicmayer42@gmail.com>
2020-04-11 20:22:17 +0100

    Sorted out the svg so the sizing all works

--- script.js
+++ script.js
@@ -25,29 +25,27 @@ function setupHighScoreText(shape) {
 function createShape(shape) {
   //In case we've done one shape reset anything
   $('#result').html(''); //Hide all the helpers in case they were previously visible
+  //$('.helper').css('visibility','hidden');
 
-  $('.helper').css('visibility', 'hidden');
   $('#reset').css('visibility', 'hidden');
   var size = getRandomIntInclusive(50, 100); //Make both the shape and the helper this size
+  //if(shape === 'square') {
 
-  if (shape === 'square') {
-    $('.shape').attr('width', size);
-    $('.shape').attr('height', size);
-    $('.helper').attr('width', size);
-    $('.helper').attr('height', size);
-  }
+  $('.svg').attr('width', size);
+  $('.svg').attr('height', size);
+  $('.helpersvg').attr('width', size);
+  $('.helpersvg').attr('height', size); //}
 
-  if (shape === 'circle') {
-    $('.shape').attr('r', size / 2);
-    $('.helper').attr('r', size / 2);
+  /*if(shape === 'circle') {
+   	$('.shape').attr('r',size/2);
+  	$('.helper').attr('r',size/2);
   }
-
-  if (shape === 'triangle') {
-    var points = [size / 2, 0, size, size, 0, size].toString('');
-    console.log(points);
-    $('.shape').attr('points', points);
-    $('.helper').attr('points', points);
-  }
+  if(shape === 'triangle') {
+  	var points = [size/2, 0, size, size, 0, size].toString('');
+  	console.log(points);
+  	$('.shape').attr('points',points);
+  	$('.helper').attr('points',points);
+  }*/
 
   $('.shape').css('cursor', 'pointer');
   $('.shape').on('click', function (event) {
@@ -58,29 +56,14 @@ function createShape(shape) {
 function growShape(shape, size) {
   $('.shape').off();
   var transitionTime = getRandomIntInclusive(2, 7);
-
-  if (shape === 'circle' || shape === 'square') {
-    $('.shape').animate({
-      'width': size * 3,
-      'height': size * 3
-    }, transitionTime * 1000, function () {
-      stopShape(shape, size);
-    });
-  }
-
-  if (shape === 'triangle') {
-    $('.shape').animate({
-      'border-left-width': size / 2 * 3,
-      'border-right-width': size / 2 * 3,
-      'border-bottom-width': size * 3
-    }, transitionTime * 1000, function () {
-      stopShape(shape, size);
-    });
-  }
-
-  $('.shape').on('click', function () {
-    $('.shape').stop(true);
-    $('.shape').off();
+  $('.svg').animate({
+    'width': size * 3,
+    'height': size * 3
+  }, transitionTime * 1000, function () {
+    stopShape(shape, size);
+  });
+  $('.shape').on('click', function (event) {
+    $('.svg').stop();
     stopShape(shape, size);
   });
 }
@@ -88,16 +71,8 @@ function growShape(shape, size) {
 function stopShape(shape, initialSize) {
   $('.shape').off();
   $('.shape').css('cursor', 'auto');
-  $('.helper').css('display', 'block');
-  var finalSize;
-
-  if (shape === 'circle' || shape === 'square') {
-    finalSize = $('.shape').width();
-  }
-
-  if (shape === 'triangle') {
-    finalSize = parseInt($('.shape').css('border-bottom-width')); //borders return with a px
-  }
+  $('.helpersvg').css('display', 'block');
+  var finalSize = $('.svg').width(); //width and height are the same, so we can just use width
 
   var ratio = (finalSize / initialSize).toFixed(1);
   var resultTextStart = "Missed!";
--- src/script.js
+++ src/script.js
@@ -24,18 +24,18 @@ function createShape(shape) {
 	//In case we've done one shape reset anything
 	$('#result').html('');
 	//Hide all the helpers in case they were previously visible
-	$('.helper').css('visibility','hidden');
+	//$('.helper').css('visibility','hidden');
 	$('#reset').css('visibility','hidden');	
   var size = getRandomIntInclusive(50, 100);
   //Make both the shape and the helper this size
-  if(shape === 'square') {
-  	$('.shape').attr('width',size);
-  	$('.shape').attr('height',size);
-  	$('.helper').attr('width',size);
-  	$('.helper').attr('height',size);
-  }
-  if(shape === 'circle') {
-  	$('.shape').attr('r',size/2);
+  //if(shape === 'square') {
+  	$('.svg').attr('width',size);
+  	$('.svg').attr('height',size);
+  	$('.helpersvg').attr('width',size);
+  	$('.helpersvg').attr('height',size);
+  //}
+  /*if(shape === 'circle') {
+   	$('.shape').attr('r',size/2);
   	$('.helper').attr('r',size/2);
   }
   if(shape === 'triangle') {
@@ -43,7 +43,7 @@ function createShape(shape) {
   	console.log(points);
   	$('.shape').attr('points',points);
   	$('.helper').attr('points',points);
-  }
+  }*/
   $('.shape').css('cursor','pointer'); 
   $('.shape').on('click', function(event) {
     growShape(event.target.id, size);
@@ -53,26 +53,14 @@ function createShape(shape) {
 function growShape(shape, size){
 	$('.shape').off();
 	var transitionTime = getRandomIntInclusive(2, 7)
-	if (shape === 'circle' || shape === 'square') {
-		$('.shape').animate({
-			'width': size * 3,
-			'height': size * 3
-		}, transitionTime * 1000, function(){
-			stopShape(shape, size);
-		}); 
-	}
-	if (shape === 'triangle') {
-		$('.shape').animate({
-			'border-left-width': size / 2 * 3,
-			'border-right-width': size / 2 * 3,
-			'border-bottom-width': size * 3
-		}, transitionTime * 1000, function(){
-			stopShape(shape, size);
-		});
-	}
-  $('.shape').on('click', function() {
-  	$('.shape').stop(true);
-  	$('.shape').off();
+	$('.svg').animate({
+		'width': size * 3,
+		'height': size * 3
+	}, transitionTime * 1000, function(){
+		stopShape(shape, size);
+	});
+  $('.shape').on('click', function(event) {
+  	$('.svg').stop();
     stopShape(shape, size);
 	});
 }
@@ -80,14 +68,8 @@ function growShape(shape, size){
 function stopShape(shape, initialSize) {
 	$('.shape').off();
 	$('.shape').css('cursor','auto');
-	$('.helper').css('display','block');
-	var finalSize;
-	if (shape === 'circle' || shape === 'square') {
-		finalSize = $('.shape').width();
-	}
-	if (shape === 'triangle') {
-		finalSize = parseInt($('.shape').css('border-bottom-width')); //borders return with a px
-	}
+	$('.helpersvg').css('display','block');
+	var finalSize = $('.svg').width(); //width and height are the same, so we can just use width
 	var ratio = (finalSize / initialSize).toFixed(1);
 	var resultTextStart = "Missed!";
 	if(ratio == 1.9 || ratio == 2.1){
